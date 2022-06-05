d30d8b845252c9ecec98d2f6749710547e0f8a97
Daniela Kesselman <72583249+dpkesselman@users.noreply.github.com>
2022-06-05 18:25:25 -0300

    Add files via upload
    Scripts de Scene02

---
+++ Scene02/ScriptWalktation2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptWalktation2 : MonoBehaviour
+{
+    Vector3 acceDir;
+    void Update()
+    {
+        acceDir = Input.acceleration;
+        if (acceDir.sqrMagnitude >=3f)
+        {
+            SceneManager.LoadScene("Scene03");
+        }        
+
+    }
+}
