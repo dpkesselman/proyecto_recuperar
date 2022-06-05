6869ffbc1ec6e5b8fa10e166b01913fec934dca6
Daniela Kesselman <72583249+dpkesselman@users.noreply.github.com>
2022-06-05 18:29:34 -0300

    Add files via upload
    Scripts de Scene06

---
+++ Scene06/ScripCursorTrenCC.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScripCursorTrenCC : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(new Vector2(Input.acceleration.x, Input.acceleration.y) * Time.deltaTime * 3f);
+    }
+}
---
+++ Scene06/ScriptTrigerTrenCC.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptTrigerTrenCC : MonoBehaviour
+{
+    int c = 0;
+    public AudioSource audioSource;
+    public GameObject pisadas;
+
+    void OnTriggerEnter (Collider other) 
+    {
+        if (other.gameObject.tag == "Triggered")
+        {
+            c++;
+            if (c < 5)
+            {
+                audioSource.Play();
+            }
+            
+            Debug.Log("Triggered" + c);
+            
+            float x = Random.Range (-5, 5);
+            float y = Random.Range (-3, 3);
+            pisadas.transform.position = new Vector2(x,y);            
+        }
+
+        if (c >= 5)
+        {
+            SceneManager.LoadScene("Scene07");
+        }
+                         
+    }
+}
