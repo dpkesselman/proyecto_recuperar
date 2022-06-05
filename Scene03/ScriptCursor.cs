656a00c1798ac5166490128175afe68a8178fdcd
Daniela Kesselman <72583249+dpkesselman@users.noreply.github.com>
2022-06-05 18:26:08 -0300

    Add files via upload
    Scrips de Scene03

---
+++ Scene03/ScriptCursor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptCursor : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(new Vector2(Input.acceleration.x, 0) * Time.deltaTime * 5f);
+    }
+}
---
+++ Scene03/ScriptTriggerSc03.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptTriggerSc03 : MonoBehaviour
+{
+
+    void OnTriggerEnter (Collider other) 
+    {
+        if(other.gameObject.tag == "Trigger")
+
+        {
+            Debug.Log ("pasa a siguiente escena");
+            SceneManager.LoadScene("Scene04");
+        }
+    }
+}
