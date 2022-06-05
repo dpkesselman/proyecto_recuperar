723c547cdef1eb653872b265d38e1e60a64f15ed
Daniela Kesselman <72583249+dpkesselman@users.noreply.github.com>
2022-06-05 18:24:26 -0300

    Add files via upload
    Scripts de Scene01

---
+++ Scene01/ScriptDontDestroyGUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptDontDestroyGUI : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] GUI = GameObject.FindGameObjectsWithTag("UI");
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Scene01/ScriptPause.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptPause : MonoBehaviour
+{
+    void  OnGUI (){
+	    if ( GUI.Button( new Rect(10,10,80,80), "►ІІ") )
+        {
+		    if(Time.timeScale == 1){    //si la velocidad es 1
+		    	Time.timeScale = 0; 	//que la velocidad del juego sea 0
+		    } 
+            else if(Time.timeScale == 0) {   // si la velocidad es 0
+			    Time.timeScale = 1;  	// que la velocidad del juego regrese a 1
+	    	}
+	    }
+    }
+}
